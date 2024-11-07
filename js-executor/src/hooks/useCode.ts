0d28396f6b62470ff226bcb16990375101b8d239
amacapozzi <amacapozzi07@gmail.com>
2024-11-07 13:16:30 -0300

    add global state to change language

--- js-executor/src/hooks/useCode.ts
+++ js-executor/src/hooks/useCode.ts
@@ -1,14 +1,23 @@
 import { create } from "zustand";
 
+export type LanguageOption = "JavaScript" | "TypeScript";
+
 type State = {
   code: string;
+  language: LanguageOption;
 };
 
 type Action = {
   setCode: (code: string) => void;
+  setLanguage: (language: string) => void;
 };
 
 export const useCodeStore = create<State & Action>()((set) => ({
   code: "console.log('Hello worlds')",
+  language: "JavaScript",
+  setLanguage: (language: string) =>
+    set(() => ({
+      language: language as LanguageOption,
+    })),
   setCode: () => set((state) => ({ code: state.code })),
 }));
